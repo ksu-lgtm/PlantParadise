5b4cdbac0de59157a76425b3e0111683e52d6bcb
ksu-lgtm <kochetkovakseniya25@gmail.com>
2026-03-30 19:54:16 +0300

    fix^ add base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PlantParadise/',   // ← добавить эту строку
+  base: '/PlantParadise/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
